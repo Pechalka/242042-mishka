e0cc973eeb1d6a8cd48b9a2f204df1b2adbae3cd
Keks <keks@htmlacademy.ru>
2017-03-09 12:27:20 +0300

    :kissing_cat: Добавил сборку sass в gulp

--- gulpfile.js
+++ gulpfile.js
@@ -10,9 +10,7 @@ var server = require("browser-sync").create();
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
     .pipe(plumber())
-    .pipe(sass({
-      includePaths: require('node-normalize-scss').includePaths
-    }))
+    .pipe(sass())
     .pipe(postcss([
       autoprefixer({browsers: [
         "last 2 versions"
