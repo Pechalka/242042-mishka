95efb378093eec725f5966ed1af7789bac8f5f2b
Andrey <luzkovskyandrey@gmail.com>
2017-03-20 00:58:20 +0300

    menu

--- js/app.js
+++ js/app.js
@@ -8,16 +8,13 @@ var Menu = function(options) {
   if (!menu) return;
 
   var button = menu.querySelector(buttonSelector);
-  var items = menu.querySelector(itemsSelector);
 
   var menuOpenClass = options.menuOpenClass;
   var buttonOpenClass = options.buttonOpenClass;
-  var itemsOpenClass = options.itemsOpenClass;
 
   button.addEventListener('click', function(){
     menu.classList.toggle(menuOpenClass);
     button.classList.toggle(buttonOpenClass);
-    items.classList.toggle(itemsOpenClass);
   })
 }
 
@@ -30,6 +27,4 @@ Menu({
   buttonSelector: '.menu__toggle',
   buttonOpenClass: 'menu__toggle--open',
 
-  itemsSelector: '.menu__list',
-  itemsOpenClass: 'menu__list--open'
 })
